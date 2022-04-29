33b1d06f732ddebe624ac42358e826015299c6c6
ElvisAns <ansimapersic@gmail.com>
2022-04-29 18:18:00 +0200

    [UPD] Update toolbox component

--- src/views/ToolBox.vue
+++ src/views/ToolBox.vue
@@ -29,7 +29,7 @@ import { defineComponent } from 'vue';
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 
 export default defineComponent({
-  name: 'FolderPage',
+  name: 'ToolBox',
   components: {
     IonButtons,
     IonContent,
