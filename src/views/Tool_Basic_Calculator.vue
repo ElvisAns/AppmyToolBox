60c34d86547d6fdef3b70b72b3272096a84eb26e
ElvisAns <ansimapersic@gmail.com>
2022-05-07 01:55:40 +0200

    [ADD] Calculator Button

--- src/views/Tool_Basic_Calculator.vue
+++ src/views/Tool_Basic_Calculator.vue
@@ -1,7 +1,38 @@
 <template>
     <div class="container">
         <div class="screen"></div>
-        <div class="keys"></div>
+        <div class="keys">
+            <div class="row">
+                <button>e</button>
+                <button>%</button>
+                <button>sin</button>
+                <button>deg</button>
+            </div>
+
+            
+            <div class="row">
+                <button>CE</button>
+                <button>(</button>
+                <button>)</button>
+                <button>/</button>
+            </div>
+
+
+            
+            <div class="row">
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>x</button>
+            </div>
+            
+            <div class="row">
+                <button>5</button>
+                <button>4</button>
+                <button>3</button>
+                <button>+</button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -25,8 +56,12 @@ export default {
         width:80vw;
     }
 }
+
+.container{
+    background-color: rgb(128, 66, 215);
+}
    .screen{
-       background-color: aqua;
+       background-color: rgb(255, 255, 255);
        height: 40px;
        width:100%;
    }
