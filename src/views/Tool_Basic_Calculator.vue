78be2dd707fa39d9f258f781c765f85c774c9fff
ElvisAns <ansimapersic@gmail.com>
2022-05-07 02:14:14 +0200

    [UX] Design Calculator Interface

--- src/views/Tool_Basic_Calculator.vue
+++ src/views/Tool_Basic_Calculator.vue
@@ -32,6 +32,22 @@
                 <button>3</button>
                 <button>+</button>
             </div>
+
+            
+            <div class="row">
+                <button>3</button>
+                <button>2</button>
+                <button>1</button>
+                <button>-</button>
+            </div>
+
+            
+            <div class="row">
+                <button>0</button>
+                <button>.</button>
+                <button>+/-</button>
+                <button>=</button>
+            </div>
         </div>
     </div>
 </template>
@@ -46,23 +62,44 @@ export default {
 <style scoped>
 .container{
     margin:20px auto;
-    width:40vw;
+    width:20vw;
     display: flex;
     justify-content: center;
+    flex-direction: column;
 }
 
-@media screen and (max-width:800px){
+
     .container{
-        width:80vw;
+        background-color: rgba(128, 66, 215,.3);
+        padding:20px;
     }
-}
-
-.container{
-    background-color: rgb(128, 66, 215);
-}
    .screen{
        background-color: rgb(255, 255, 255);
        height: 40px;
        width:100%;
+   }
+
+   button{
+       padding:6px;
+       text-align: center;
+       margin:5px;
+       border-radius: 4px;
+       width:calc(20vw/4);
+       height: 40px;
+   }
+   
+@media screen and (max-width:800px){
+    .container{
+        width:80vw;
+    }
+    button{
+       width:calc(80vw/4);
+   }
+
+}
+
+   .row{
+       display: flex;
+       width: auto;
    }
 </style>
